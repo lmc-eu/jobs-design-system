6bf6c2a89767b874daf0e80949ef1039b6647987
Adam Kudrna <adam@adamkudrna.cz>
2022-02-28 23:00:46 +0100

    Chore: Introduce basic configured dev stack

---
+++ .commitlintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  extends: ['@lmc-eu/commitlint-config'],
+  ignores: [
+    (commit) => commit.includes('[ci-skip]'),
+    (commit) => commit.includes('Pull request'),
+    (commit) => commit === 'Updated styles and tokens',
+  ],
+  rules: {
+    'scope-enum': [
+      1,
+      'always',
+      [
+        // Use when affecting CI process
+        'ci',
+        // Use for anything that does not directly affect packages, ie. updating repo-wide
+        'repo',
+      ],
+    ],
+  },
+};
