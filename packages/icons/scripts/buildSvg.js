f8592f1056db84b8ddfcf8bc86f9b11a9c5dacd9
literat <tomas@litera.me>
2022-09-02 12:27:27 +0200

    Chore(icons): Fix build script whe there are no files

--- packages/icons/scripts/buildSvg.js
+++ packages/icons/scripts/buildSvg.js
@@ -6,26 +6,29 @@ const svgDistDir = path.resolve(__dirname, `../dist/svg`);
 
 const normalizeAndCopySvg = (srcDir, distDir) => {
   fs.readdir(srcDir, (err, files) => {
-    const svgs = files.filter((el) => path.extname(el) === '.svg');
-    if (svgs.length > 0) {
-      let sprite = '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">\n';
-      const spriteDistFile = path.join(distDir, 'sprite.svg');
+    if (files?.length > 0) {
+      const svgs = files.filter((el) => path.extname(el) === '.svg');
 
-      svgs.forEach((svg) => {
-        const svgPath = path.join(srcDir, svg);
-        const svgDistPath = path.join(distDir, svg);
-        const svgContent = fs.readFileSync(svgPath, 'utf8');
-        const svgContentFixed = svgContent.replace(/fill="#\w+"/g, 'fill="currentColor"');
-        const svgSpriteContent = svgContentFixed
-          .replace(/<svg.*(viewBox="(\d+\s){3}\d+").*>/, `<symbol id="${svg.slice(0, -4)}" $1>`)
-          .replace(/<\/svg>/g, '</symbol>');
-        sprite += svgSpriteContent;
-        fs.writeFileSync(path.join(svgDistPath), svgContentFixed);
-      });
+      if (svgs.length > 0) {
+        let sprite = '<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">\n';
+        const spriteDistFile = path.join(distDir, 'sprite.svg');
 
-      sprite += '</svg>';
+        svgs.forEach((svg) => {
+          const svgPath = path.join(srcDir, svg);
+          const svgDistPath = path.join(distDir, svg);
+          const svgContent = fs.readFileSync(svgPath, 'utf8');
+          const svgContentFixed = svgContent.replace(/fill="#\w+"/g, 'fill="currentColor"');
+          const svgSpriteContent = svgContentFixed
+            .replace(/<svg.*(viewBox="(\d+\s){3}\d+").*>/, `<symbol id="${svg.slice(0, -4)}" $1>`)
+            .replace(/<\/svg>/g, '</symbol>');
+          sprite += svgSpriteContent;
+          fs.writeFileSync(path.join(svgDistPath), svgContentFixed);
+        });
 
-      fs.writeFileSync(spriteDistFile, sprite);
+        sprite += '</svg>';
+
+        fs.writeFileSync(spriteDistFile, sprite);
+      }
     }
   });
 };
