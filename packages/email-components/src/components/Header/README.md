37b2adf09e0d8c45b2095c14babde2857fbd9e2c
Chadima Ondrej <Chadima.ondrej@gmail.com>
2023-06-09 14:08:48 +0200

    Feat(email-components): Add Rating component

--- packages/email-components/src/components/Header/README.md
+++ packages/email-components/src/components/Header/README.md
@@ -12,11 +12,11 @@ The `mjc-header` is component for e-mail header with company logo.
 
 ## Available props
 
-| Prop name      | Type                      | Default  | Required | Description                    |
-| -------------- | ------------------------- | -------- | -------- | ------------------------------ |
-| `align`        | `left \| center \| right` | `center` | no       | Image aligment                 |
-| `height`       | `unit(px,%)`              | `24px`   | no       | Image height                   |
-| `externalLink` | `string`                  | -        | no       | External link to some web page |
-| `imageAlt`     | `string`                  | -        | no       | Image alt description          |
-| `imageUrl`     | `string`                  | -        | yes      | Url to image                   |
-| `width`        | `unit(px,%)`              | `60px`   | no       | Image width                    |
+| Prop name      | Type                      | Default  | Required | Description                        |
+| -------------- | ------------------------- | -------- | -------- | ---------------------------------- |
+| `align`        | `left \| center \| right` | `center` | no       | Image aligment                     |
+| `height`       | `unit(px,%)`              | `24px`   | no       | Image height                       |
+| `externalLink` | `string`                  | -        | no       | External link to some web page     |
+| `imageAlt`     | `string`                  | -        | no       | Image alternative descriptive text |
+| `imageUrl`     | `string`                  | -        | yes      | Url to image                       |
+| `width`        | `unit(px,%)`              | `60px`   | no       | Image width                        |
---
+++ packages/email-components/src/components/RatingItem/RatingItem.ts
@@ -0,0 +1,70 @@
+import { BodyComponent } from 'mjml-core';
+import { loadComponentStyles } from '../../scssLoaders/loadComponentStyles';
+
+export default class RatingItem extends BodyComponent {
+  private readonly alt: string;
+  private readonly href: string;
+  private readonly rel: string;
+  private readonly src: string;
+  private readonly title: string;
+
+  constructor(initialData = {}) {
+    super(initialData);
+
+    this.alt = this.getAttribute('alt');
+    this.href = this.getAttribute('href');
+    this.rel = this.getAttribute('rel');
+    this.src = this.getAttribute('src');
+    this.title = this.getAttribute('title');
+  }
+
+  static componentName = 'mjc-rating-item';
+  static endingTag = true;
+  static dependencies = {
+    'mj-column': ['mjc-rating-item'],
+    'mj-rating': ['mjc-rating-item'],
+    'mjc-rating-item': [],
+  };
+
+  static allowedAttributes = {
+    alt: 'string',
+    href: 'string',
+    isTableCell: 'boolean',
+    rel: 'string',
+    src: 'string',
+    title: 'string',
+  };
+
+  headStyle = () => loadComponentStyles(`${__dirname}/RatingItem.css`);
+
+  generateTableCell = (content: any) => {
+    if (this.getAttribute('isTableCell')) {
+      const tableCellClass = `RatingItem${this.props.last ? ' RatingItem--last' : ''}`;
+      return `<td class="${tableCellClass}">${content}</td>`;
+    }
+
+    return content;
+  };
+
+  render() {
+    return this.generateTableCell(`
+      <a
+        ${this.htmlAttributes({
+          href: this.href,
+          rel: this.rel,
+          target: '_blank',
+          class: 'RatingItem__link',
+        })}
+      >
+        <img
+          ${this.htmlAttributes({
+            alt: this.alt,
+            class: 'RatingItem__icon',
+            src: this.src,
+          })}
+        />
+        ${this.title && `<div class="RatingItem__text">${this.title}</div>`}
+      </a>
+    `);
+  }
+}
